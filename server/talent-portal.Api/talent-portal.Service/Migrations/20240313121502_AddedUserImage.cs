4906d1182db7819372293ad41e4f99734fcb26d8
Mahesh P <pmahesh072002@gmail.com>
2024-03-23 13:06:50 +0530

    migrations

---
+++ server/talent-portal.Api/talent-portal.Service/Migrations/20240313121502_AddedUserImage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace talent_portal.Service.Migrations
+{
+    public partial class AddedUserImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserImage",
+                table: "Results",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserImage",
+                table: "Results");
+        }
+    }
+}
